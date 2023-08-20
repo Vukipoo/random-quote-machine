d9a63a53dc6fbb216d6b255c30d5546d3bebddaa
Vukipoo <perovicv515@gmail.com>
2023-08-20 00:03:00 -0700

    add quotes api, have it render

---
+++ random-quote-machine/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [quote, setQuote] = useState("");
+
+  const getQuote = () => {
+    fetch("https://api.themotivate365.com/stoic-quote")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
+      });
+    }
+
+  
+  
+
+  return (
+    <div className="App">
+      <button onClick={getQuote}>Get Quote</button>
+      <div>{quote.author}</div>
+      <div>{quote.quote}</div>
+
+    </div>
+  );
+}
